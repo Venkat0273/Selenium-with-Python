cce22a8b6424b9707dee08819e789a2b57cd893b
Venkat0273 <69948914+Venkat0273@users.noreply.github.com>
2020-08-20 15:50:03 +0530

    Update PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,1 +1,23 @@
+"""Let's write some basic stuff in Python 3.x"""
+# When ever you are writing functions in Python, make function name in lower case itself else python raises PEP8 Error though it is not functional error but complying with PEP8 standard is recommended
+def python_basics():
+  """Let's write some stuff regarding boolean data types.
+  Operations that we can perform on Booleans are AND, OR and NOT. These are logical operators.
+  We can also check whether bool is sub class of int.
+  Let's have a practical implementation of the above discussed
+  """
+  # Let's declare 2 variables
+  a, b = True, False
+  # Let's perform AND operation on the aforeassigned variables. Logical AND returns True if both the operands are True else it returns False. In our case, a is True but b is False hence it returns False
+  print(f"Are they equal?: {a AND b}")
+  # Let's perform OR operation. OR operator returns True if any one of the operand is True.
+  print(f"Are they equal?: {a OR b}")
+  # Let's perform NOT operation. NOT returns negative of boolean values. If input is True, it returns False.
+  print(f"Is it True?: {NOT b}")
+  print(f"Is bool sub class of int?: {issubclass(bool, int)}")
+  print(f"Is True instance of bool?: {isinstance(True, bool)}")
+  print(f"Is False instance of bool?: {isinstance(False, bool)}")
+  print("We are done with Boolean operations, change this file to add few more if you know")
 
+
+python_basics()
